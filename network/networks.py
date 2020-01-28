0974dcaf2b1e99c23fa5a510cb19d6ba583ad55c
Tony <anthony@amartinez.cc>
2020-01-28 13:40:50 -0800

    add helper functions

---
+++ network/networks.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+
+from utils.net_helper import *
+
+
---
+++ utils/net_helper.py
@@ -0,0 +1,29 @@
+
+import torch
+import torch.nn as nn
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+    
+def conv_layer(chanIn, chanOut, kernel_size = 3,strd=1, padding=1, drop=.04):
+    return nn.Sequential(
+        nn.Conv2d(chanIn, chanOut, kernel_size,stride=strd, padding=padding),        
+        nn.BatchNorm2d(chanOut),
+        nn.ReLU(),
+        nn.Dropout(drop)
+        )
+
+def discr_dense(chanIn):
+    return nn.Sequential(
+        Flatten(),
+        nn.Linear(chanIn, 1),
+        nn.Sigmoid() #...bcewlogits already has a sigmoid built in
+        )
+
+def gen_dense(chanIn, chanOut):
+    return nn.Sequential(
+        nn.Linear(chanIn, chanOut),
+#         nn.BatchNorm2d(chanOut),
+        nn.ReLU()
+        )
